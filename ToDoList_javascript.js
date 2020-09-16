f4a1434248af1ee3847beaa0f9681851131e5bfc
KletnickiLjupce <KletnickiLjupce@users.noreply.github.com>
2020-09-17 01:14:56 +0200

    commit
    Full app commit

---
+++ ToDoList_javascript.js
@@ -0,0 +1,5 @@
+// check off specifiv todos by clickng
+
+$("li").click(function(){
+	console.log("clicked Li");
+})
---
+++ assets/js/ToDoList_javascript.js
@@ -0,0 +1,30 @@
+// check off specifiv todos by clickng
+$("ul").on("click","li", function(){
+	$(this).toggleClass("completed");
+});
+// click on X to delete event
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		// when enter is hit, grabs the new text
+		var todoText=$(this).val();
+		$(this).val("");
+		//create a nre li and add to ul
+		$("ul").append("<li>"+ "<span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+
+	}})
+$('.fa-plus').click(function(){
+	
+	$("input[type='text'").fadeToggle();
+})
+
+
+
+	
